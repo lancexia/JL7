c84aa2b084d6d879470281ef09c0d019fb19dbc7
夏凯伦 <lance@XL.local>
2017-07-11 05:30:12 +0800

    create resume attchment to job and user

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,6 +3,7 @@ class Job < ApplicationRecord
   validates :wage_upper_bound, presence: true
   validates :wage_lower_bound, presence: true
   validates :wage_lower_bound, numericality: { greater_than: 0}
+  has_many :resumes
 
   def publish!
     self.is_hidden = false
@@ -14,6 +15,6 @@ class Job < ApplicationRecord
     self.save
   end
 
-  scope :pulished, -> { where(is_hidden: false)}
+  scope :published, -> { where(is_hidden: false)}
   scope :recent, -> {order('created_at DESC')}
 end
